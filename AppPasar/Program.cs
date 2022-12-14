b79fbc1f2b6f79f4c4d8e2204055d2e1e963e622
Fitraldy Soefana <fitraldy.soefana@gmail.com>
2022-12-14 08:16:57 +0700

    [Update] Update API

--- AppPasar/Program.cs
+++ AppPasar/Program.cs
@@ -1,27 +1,27 @@
-﻿using System.Data.SqlClient;
-using System.Data.Common;
-class Program
-{
-    static void Main()
-    {
-        SqlConnection conn = null;
-        try
-        {
-            conn = new SqlConnection(@"server=HAPIPILO\SQLEXPRESS;Database=CompanyDB;Trusted_Connection=True;"); //connection database
-            //Native Query
-            SqlCommand cmd = new SqlCommand(@"create table Distributor (PenjualName varchar(100), Barang varchar(50), Harga int not null)", conn);
-            SqlCommand cmd2 = new SqlCommand(@"create table Pembeli (PembeliName varchar(100), Barang varchar(50), Harga int not null)", conn);
+﻿//using System.Data.SqlClient;
+//using System.Data.Common;
+//class Program
+//{
+//    static void Main()
+//    {
+//        SqlConnection conn = null;
+//        try
+//        {
+//            conn = new SqlConnection(@"server=HAPIPILO\SQLEXPRESS;Database=CompanyDB;Trusted_Connection=True;"); //connection database
+//            //Native Query
+//            SqlCommand cmd = new SqlCommand(@"create table Distributor (PenjualName varchar(100), Barang varchar(50), Harga int not null)", conn);
+//            SqlCommand cmd2 = new SqlCommand(@"create table Pembeli (PembeliName varchar(100), Barang varchar(50), Harga int not null)", conn);
 
-            conn.Open();
-            cmd.ExecuteNonQuery();
-            cmd2.ExecuteNonQuery();
-            Console.WriteLine("Connect and create succesfull");
-        }
-        finally
-        {
-            conn.Close();
-            Console.WriteLine("close connection!!");
-        }
+//            conn.Open();
+//            cmd.ExecuteNonQuery();
+//            cmd2.ExecuteNonQuery();
+//            Console.WriteLine("Connect and create succesfull");
+//        }
+//        finally
+//        {
+//            conn.Close();
+//            Console.WriteLine("close connection!!");
+//        }
 
-    }
-}
+//    }
+//}
